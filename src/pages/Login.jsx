73b898531e367064827606bc04afb1f0407faded
Raghav <raghav@Raghavs-MacBook-Pro.local>
2024-10-14 16:47:30 +0530

    login done

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,13 +1,50 @@
-import React from 'react'
-import { Card, CardBody,Text } from '@chakra-ui/react'
+import { Button, Card, CardBody, FormControl, FormLabel, Input, Stack } from '@chakra-ui/react';
+import React, { useState } from 'react';
 
 const Login = () => {
+  const [formInput, setFormInput] = useState({ email: '', password: '' })
+
+  const handleFormChange = (e) => {
+    const { name, value } = e.target;
+    setFormInput(prev => ({ ...prev, [name]: value }));
+  };
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+    console.log('Form submitted with:', formInput);
+  }
   return (
     <Card>
-    <CardBody>
-      <Text>Login</Text>
-    </CardBody>
-  </Card>
+      <CardBody>
+        <form onSubmit={handleFormSubmit}>
+          <FormControl isRequired>
+            <FormLabel>Email</FormLabel>
+            <Input
+              m={'5px'}
+              type='email'
+              name='email'
+              value={formInput.email}
+              onChange={handleFormChange}
+            />
+          </FormControl>
+          <FormControl isRequired>
+            <FormLabel>Password</FormLabel>
+            <Input
+              m={'5px'}
+              type='password'
+              name='password'
+              value={formInput.password}
+              onChange={handleFormChange}
+            />
+          </FormControl>
+          <Stack>
+            <Button type='submit' m={'5px'} colorScheme='teal'>
+              Login
+            </Button>
+          </Stack>
+        </form>
+      </CardBody>
+    </Card>
   )
 }
 
--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,13 +1,50 @@
-import { Card, CardBody,Text } from '@chakra-ui/react'
-import React from 'react'
+import React, { useState } from 'react'
+import { Button, Card, CardBody, FormControl, FormLabel, Input, Stack } from '@chakra-ui/react';
 
 const SignUp = () => {
+  const [formInput, setFormInput] = useState({ email: '', password: '' })
+
+  const handleFormChange = (e) => {
+    const { name, value } = e.target;
+    setFormInput(prev => ({ ...prev, [name]: value }));
+  };
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+    console.log('Form submitted with:', formInput);
+  }
   return (
     <Card>
-    <CardBody>
-      <Text>Signup</Text>
-    </CardBody>
-  </Card>
+      <CardBody>
+        <form onSubmit={handleFormSubmit}>
+          <FormControl isRequired>
+            <FormLabel>Email</FormLabel>
+            <Input
+              m={'5px'}
+              type='email'
+              name='email'
+              value={formInput.email}
+              onChange={handleFormChange}
+            />
+          </FormControl>
+          <FormControl isRequired>
+            <FormLabel>Password</FormLabel>
+            <Input
+              m={'5px'}
+              type='password'
+              name='password'
+              value={formInput.password}
+              onChange={handleFormChange}
+            />
+          </FormControl>
+          <Stack>
+            <Button type='submit' m={'5px'} colorScheme='teal'>
+              Login
+            </Button>
+          </Stack>
+        </form>
+      </CardBody>
+    </Card>
   )
 }
 
